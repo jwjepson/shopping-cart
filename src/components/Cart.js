65d85c59829670f35f4ad8f2b3c24a3aa82a7ebe
Jake Jepson <jwjepson1@gmail.com>
2023-02-27 18:18:37 -0500

    wrap cart in main div

--- src/components/Cart.js
+++ src/components/Cart.js
@@ -2,7 +2,9 @@ import React from "react";
 
 const Cart = () => {
     return (
-        <h2>Shopping Cart</h2>
+        <div className="main">
+            <h2>Shopping Cart</h2>
+        </div>
     )
 }
 
