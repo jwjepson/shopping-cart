9a9f0445d49e32f393480fd13432320f13100dfd
Jake Jepson <jwjepson1@gmail.com>
2023-02-27 18:20:04 -0500

    create footer component

---
+++ src/components/Footer.js
@@ -0,0 +1,9 @@
+import React from "react";
+
+const Footer = () => {
+    return (
+        <footer>Built by <a href="https://github.com/jwjepson">jwjepson</a> | <a href="https://github.com/jwjepson/memory-card">Source Code</a></footer>
+    )
+}
+
+export default Footer;
