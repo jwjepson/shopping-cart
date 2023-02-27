4501eae865cfdfc0d23acae6922fadf9c715c286
Jake Jepson <jwjepson1@gmail.com>
2023-02-27 17:46:09 -0500

    Link to the correct path in nav bar

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,13 +1,18 @@
 import React from "react";
 import "../styles/Header.css";
+import { Link } from "react-router-dom";
 
 const Header = () => {
     return (
         <div className="header">
             <div className="logo">Logo</div>
             <ul className="nav-bar">
-                <li>Home</li>
-                <li>Shop</li>
+                <Link to="/">
+                    <li>Home</li>
+                </Link>
+                <Link to="/shop">
+                    <li>Shop</li>
+                </Link>
             </ul>
             <div className="shopping-cart">Cart (0)</div>
         </div>
