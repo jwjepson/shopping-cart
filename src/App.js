6a58b4bac506bfcacc0e59a2f50a7bac7c884cd0
Jake Jepson <jwjepson1@gmail.com>
2023-02-27 17:45:44 -0500

    set up routes for home and shop pages

--- src/App.js
+++ src/App.js
@@ -1,9 +1,18 @@
 import Header from "./components/Header";
+import { BrowserRouter, Routes, Route} from "react-router-dom";
+import Home from "./components/Home";
+import Shop from "./components/Shop";
 
 function App() {
   return (
     <>
+      <BrowserRouter>
       <Header/>
+          <Routes>
+              <Route path="/" element={<Home/>}/>
+              <Route path="/shop" element={<Shop/>}/>
+          </Routes>
+      </BrowserRouter>
     </>
   );
 }
