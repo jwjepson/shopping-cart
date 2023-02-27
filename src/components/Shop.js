38b29777ddd9a4b34c00799cd4c165a38a47fabc
Jake Jepson <jwjepson1@gmail.com>
2023-02-27 18:19:14 -0500

    wrap shop in main div

--- src/components/Shop.js
+++ src/components/Shop.js
@@ -2,7 +2,9 @@ import React from "react";
 
 const Shop = () => {
     return (
-        <h2>Welcome to the Shop!</h2>
+        <div className="main">
+            <h2>Welcome to the Shop!</h2>
+        </div>
     )
 }
 
