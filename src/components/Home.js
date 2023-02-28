5ce9e9a7aee0315833b108ef5f63b56c20ff4258
Jake Jepson <jwjepson1@gmail.com>
2023-02-28 18:05:11 -0500

    create initial homepage with title, description, menu button and image

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,10 +1,21 @@
 import React from "react";
 import "../styles/Home.css";
+import DonutSplash from "../images/donut-splash.png";
 
 const Home = () => {
     return (
         <div className="main">
-            <h2>This is the home page</h2>
+            <div className="home">
+                <div className="left-info">
+                    <div className="home-title">Welcome to Chef's Bakery</div>
+                    <div className="home-description">Indulge in our delectable donuts freshly made every day!
+                        Our bakery shop offers a wide variety of flavors that will surely satisfy
+                        your sweet cravings.
+                    </div>
+                    <button className="menu-button" type="button">Menu</button>
+                </div>
+                <img className="donut-splash" src={DonutSplash} alt="Picture of donuts"></img>
+            </div>
         </div>
     )
 }
