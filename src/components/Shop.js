e404b564d9b759c314b267ce21ee67e046aec989
Jake Jepson <jwjepson1@gmail.com>
2023-02-28 19:33:40 -0500

    render shop items by importing item component

--- src/components/Shop.js
+++ src/components/Shop.js
@@ -1,9 +1,19 @@
 import React from "react";
+import Item from "./Item";
+import "../styles/Shop.css";
 
 const Shop = () => {
     return (
         <div className="main">
-            <h2>Welcome to the Shop!</h2>
+            <div className="menu">
+                <h2>Welcome to the Shop!</h2>
+                <div className="items">
+                    <Item/>
+                    <Item/>
+                    <Item/>
+                    <Item/>
+                </div>
+            </div>
         </div>
     )
 }
