16b634bef2cfee27536a3652425140bde31700c2
Jake Jepson <jwjepson1@gmail.com>
2023-03-01 00:06:04 -0500

    dynamically render items in cart using props

--- src/components/Cart.js
+++ src/components/Cart.js
@@ -1,9 +1,29 @@
 import React from "react";
+import "../styles/Cart.css";
 
-const Cart = () => {
+const Cart = (props) => {
     return (
         <div className="main">
-            <h2>Shopping Cart</h2>
+            <div className="cart">
+                <h2>Shopping Cart</h2>
+                <div className="cart-items">
+                    <div className="cart-headers">
+                        <div className="item-name">Item</div>
+                        <div className="item-quantity">Quantity</div>
+                        <div className="item-price">Price</div>
+                    </div>
+                    {props.cartItems.map((item) => (
+                        <div className="cart-item">
+                            <div className="item-name">
+                                <img className="cart-item-image" alt={item.name} src={item.img}></img>
+                                <div>{item.name}</div>
+                            </div>
+                            <div className="item-quantity">{item.quantity}</div>
+                            <div className="item-price">{item.price}</div>
+                        </div>
+                    ))}
+                </div>
+            </div>
         </div>
     )
 }
