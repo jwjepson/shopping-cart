5b2d19fb1c2b71d4c3a63b430069ccd4e3808835
Jake Jepson <jwjepson1@gmail.com>
2023-02-28 21:21:56 -0500

    create cartCount using state and increment count function

--- src/App.js
+++ src/App.js
@@ -4,15 +4,22 @@ import Footer from "./components/Footer";
 import Home from "./components/Home";
 import Shop from "./components/Shop";
 import Cart from "./components/Cart";
+import React, {useState} from "react";
 
-function App() {
+function App() { 
+  const [cartCount, setCartCount] = useState(0);
+
+  const incrementCartCount = () => {
+    setCartCount(cartCount + 1);
+  };
+
   return (
     <div className="App">
       <BrowserRouter>
-      <Header/>
+      <Header cartCount={cartCount}/>
           <Routes>
               <Route path="/" element={<Home/>}/>
-              <Route path="/shop" element={<Shop/>}/>
+              <Route path="/shop" element={<Shop incrementCartCount={incrementCartCount}/>}/>
               <Route path="/cart" element={<Cart/>}/>
           </Routes>
       </BrowserRouter>
